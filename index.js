b6562c214188bbedca0b32b265f51e20b13b852f
khalilulah <alausakhalil@gmail.com>
2025-08-11 05:04:42 -0900

    solving...

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.json({ message: "API is working!" });
+});
+
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 app.use("/api/auth", authroute);
 app.use("/api/products", productRoute);
 app.use("/api/cart", cartRoute);
